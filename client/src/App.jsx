aa5faaa7c69ff60bbb3e23279331fd7b2e8d5fa0
Gazal Mehrotra <gazalmehrotra883@gmail.com>
2023-05-28 00:26:59 +0530

    Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./assets/theme";
 import "./App.css";
+import Analyse from "./components/Analyse";
 
 import Navbar from "./components/navbar";
 import { CssBaseline, Container } from "@mui/material";
 import Hero from "./components/Hero";
+import Calendar from "./components/Calendar";
+import Mood from "./components/Mood";
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Container maxWidth="lg">
           <Navbar />
           <Hero />
+          <Calendar />
+          <Mood />
+          <Analyse />
         </Container>
       </ThemeProvider>
     </React.Fragment>
